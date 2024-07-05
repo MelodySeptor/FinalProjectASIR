0273ed39d92760f1a619c64bf2e67bc6c94e4415
Melody <polgn1995@gmail.com>
2024-07-05 18:59:07 +0200

    CSS, pug views and basic index recreated

---
+++ index.js
@@ -0,0 +1,39 @@
+﻿/*
+*   @Authors: Carmen Alvarez
+*   @Version: 1.0
+*   @Date: 2024/07/05
+*   Index file who content all the imports and configuration of server.
+*/
+
+//Init variables
+const express = require('express')
+const app = express()
+var session = require('express-session')
+var helmet = require('helmet')
+const bodyParser = require("body-parser");
+const port = 8080
+var redis = require("redis"), client = redis.createClient();
+
+//Import all dependecies
+require('./private/Register_Login/Register')
+require('./private/Register_Login/Login')
+require('./private/datosVariables')
+require('./private/Chat/preChat')
+require('./private/Administr/administrLoads')
+require('./private/Administr/adminTools')
+require('./private/Register_Login/GestorPassUser')
+require('./private/DataBase/Redis')
+
+//Start view engine and session
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+var sessionMiddleware = session({secret:'ASIR', resave:false, saveUninitialized:false})
+app.use(sessionMiddleware)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(helmet())
+app.set('views', __dirname + '/public/views')
+
+const server = app.listen(port, () =>{
+    console.log("Adotpme iniciado en: " + port)
+ })
